cc51d6e43fe6c7ac8a8ce47b5bf84e71e7b08b39
jcmier1014 <jcmier1014@gmail.com>
2018-08-30 23:34:48 -0400

    Add project files.

---
+++ MVCBartenderApp/Controllers/BartenderAppController.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Web.Mvc
+
+Namespace Controllers
+    Public Class BartenderAppController
+        Inherits Controller
+
+        ' GET: BartenderApp
+        Function Index() As ActionResult
+            Return View()
+        End Function
+    End Class
+End Namespace
---
+++ WebApplication1/Controllers/BartenderAppController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class BartenderAppController : Controller
+    {
+        // GET: BartenderApp
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
